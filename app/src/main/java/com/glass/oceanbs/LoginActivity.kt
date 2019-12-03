426f6117334fc34dee8391becf324a6c817fcf01
juargore <arturo.g.resendiz@hotmail.com>
2019-12-02 20:36:42 -0600

    Splash Screen design

--- app/src/main/java/com/glass/oceanbs/LoginActivity.kt
+++ app/src/main/java/com/glass/oceanbs/LoginActivity.kt
@@ -1,12 +1,34 @@
 package com.glass.oceanbs
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.inputmethod.InputMethodManager
+import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var parentLayoutLogin: LinearLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        supportActionBar?.hide()
+
+        initComponents()
+    }
+
+    private fun initComponents(){
+        parentLayoutLogin = findViewById(R.id.parentLayoutLogin)
+    }
+
+    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
+        if (currentFocus != null) {
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(currentFocus!!.windowToken, 0)
+        }
+        return super.dispatchTouchEvent(ev)
     }
 }
