fde2a93eee24b84ef4a122f171369a957f2c43d1
Arturo Gomez <arturog@bluetrailsoft.com>
2022-11-10 12:53:15 -0600

    feat: Documentation fragment UI finished

--- app/src/main/java/com/glass/oceanbs/fragments/aftermarket/DocumentationFragment.kt
+++ app/src/main/java/com/glass/oceanbs/fragments/aftermarket/DocumentationFragment.kt
@@ -1,22 +1,45 @@
 package com.glass.oceanbs.fragments.aftermarket
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
 import com.glass.oceanbs.R
 
 class DocumentationFragment : Fragment() {
+
+    private var root: View? = null
 
     companion object {
         fun newInstance() = DocumentationFragment()
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_documentation, container, false)
+    override fun onCreateView(infl: LayoutInflater, cont: ViewGroup?, state: Bundle?): View? {
+        if (root == null) {
+            root = infl.inflate(R.layout.fragment_documentation, cont, false)
+            setupViews()
+        }
+        return root
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun setupViews() {
+        root?.findViewById<Button>(R.id.btnYes)?.setOnClickListener {
+            // todo yes
+        }
+        root?.findViewById<Button>(R.id.btnNo)?.setOnClickListener {
+            // todo no
+        }
+        val progress = 32 // todo: set real percentage from Server
+        val progressBar = root?.findViewById<View>(R.id.circularProgress)
+        val str = progressBar?.findViewById<TextView>(R.id.progress_tv)
+        val pr = progressBar?.findViewById<ProgressBar>(R.id.circular_determinative_pb)
+        str?.text = "$progress%"
+        pr?.progress = progress
     }
 }
